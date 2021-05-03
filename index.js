9331de143cbe6f03aff54906f48aee3937733de9
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-05-03 16:16:12 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function superbowlWin(record) {
+    const r = record.find((answer) => answer.result === 'W')
+    if (r)
+    return r.year
+}
 
+// const ridiculous = record.find(({year}) => year === '1969')
+
+// function superbowlWin(ridiculous) {
+//         return ridiculous.year
+// }
